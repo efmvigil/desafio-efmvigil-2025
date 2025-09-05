c930c1fa6a17f0bd82a4964a1c32becf20d8eb29
efmvigil <efmvigil@gmail.com>
2025-09-05 18:01:02 -0300

    Adicionados o método de mostrar brinquedos e arquivo de animais disponíveis para adoção

--- src/abrigo-animais.js
+++ src/abrigo-animais.js
@@ -1,6 +1,36 @@
+import animaisDisponiveis from './animais-disponiveis-adocao';
+
 class AbrigoAnimais {
+  mostraBrinquedos(brinquedos, animal) {
+    const brinquedosCorretosParaEsseAnimal = [];
+
+    brinquedos.forEach((brinquedo) => {
+      if (animal.brinquedosFavoritos.includes(brinquedo)) {
+        brinquedosCorretosParaEsseAnimal.push(brinquedo);
+      }
+    });
+
+    if (
+      brinquedosCorretosParaEsseAnimal.length ===
+        animal.brinquedosFavoritos.length &&
+      brinquedosCorretosParaEsseAnimal.every(
+        (brinquedo, i) => brinquedo === animal.brinquedosFavoritos[i]
+      )
+    ) {
+      return true;
+    } else return false;
+  }
 
   encontraPessoas(brinquedosPessoa1, brinquedosPessoa2, ordemAnimais) {
+    const listaBrinquedosPessoa1 = brinquedosPessoa1.split(',');
+    const listaBrinquedosPessoa2 = brinquedosPessoa2.split(',');
+    const listaAnimais = ordemAnimais
+      .split(',')
+      .map((nomeAnimal) =>
+        animaisDisponiveis.find(
+          (animalDisponivel) => nomeAnimal === animalDisponivel.nome
+        )
+      );
   }
 }
 
---
+++ src/animais-disponiveis-adocao.js
@@ -0,0 +1,39 @@
+animaisDisponiveis = [
+  {
+    nome: 'Rex',
+    especie: 'cão',
+    brinquedosFavoritos: ['RATO', 'BOLA'],
+  },
+  {
+    nome: 'Mimi',
+    especie: 'gato',
+    brinquedosFavoritos: ['BOLA', 'LASER'],
+  },
+  {
+    nome: 'Fofo',
+    especie: 'gato',
+    brinquedosFavoritos: ['BOLA', 'RATO', 'LASER'],
+  },
+  {
+    nome: 'Zero',
+    especie: 'gato',
+    brinquedosFavoritos: ['RATO', 'BOLA'],
+  },
+  {
+    nome: 'Bola',
+    especie: 'cão',
+    brinquedosFavoritos: ['CAIXA', 'NOVELO'],
+  },
+  {
+    nome: 'Bebe',
+    especie: 'cão',
+    brinquedosFavoritos: ['LASER', 'RATO', 'BOLA'],
+  },
+  {
+    nome: 'Loco',
+    especie: 'jabuti',
+    brinquedosFavoritos: ['SKATE', 'RATO'],
+  },
+];
+
+export { animaisDisponiveis as animaisDisponiveis };
