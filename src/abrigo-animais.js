fb32e1abde2562fa025769409a47e395f59bf603
efmvigil <efmvigil@gmail.com>
2025-09-09 23:16:30 -0300

    Refatoração de código

--- src/abrigo-animais.js
+++ src/abrigo-animais.js
@@ -1,14 +1,23 @@
 import { animaisDisponiveis } from './animais-disponiveis-adocao.js';
 
+const BRINQUEDOS_VALIDOS = new Set(
+  animaisDisponiveis.reduce(
+    (acumulador, animal) => [...acumulador, ...animal.brinquedosFavoritos],
+    []
+  )
+);
+
 class AbrigoAnimais {
   encontraPessoas(brinquedosPessoa1, brinquedosPessoa2, ordemAnimais) {
     const listaBrinquedosPessoa1 = brinquedosPessoa1.split(',');
     const listaBrinquedosPessoa2 = brinquedosPessoa2.split(',');
     if (
-      listaBrinquedosPessoa1.length != new Set(listaBrinquedosPessoa1).size ||
-      listaBrinquedosPessoa2.length != new Set(listaBrinquedosPessoa2).size ||
-      this.verificaBrinquedoInvalido(listaBrinquedosPessoa1) ||
-      this.verificaBrinquedoInvalido(listaBrinquedosPessoa2)
+      listaBrinquedosPessoa1.length !== new Set(listaBrinquedosPessoa1).size ||
+      listaBrinquedosPessoa2.length !== new Set(listaBrinquedosPessoa2).size ||
+      this.verificaBrinquedoInvalido(
+        listaBrinquedosPessoa1,
+        listaBrinquedosPessoa2
+      )
     ) {
       return { erro: 'Brinquedo inválido' };
     }
@@ -31,28 +40,25 @@ class AbrigoAnimais {
     const animaisAdotadosPessoa2 = [];
 
     listaAnimais.forEach((animal) => {
-      const pessoa1PodeAdotar = this.verificaBrinquedosCorretos(
+      const pessoa1BrinquedosCorretos = this.verificaBrinquedosCorretos(
         listaBrinquedosPessoa1,
         animal,
         animaisAdotadosPessoa1
       );
-      const pessoa2PodeAdotar = this.verificaBrinquedosCorretos(
+      const pessoa2BrinquedosCorretos = this.verificaBrinquedosCorretos(
         listaBrinquedosPessoa2,
         animal,
         animaisAdotadosPessoa2
       );
 
-      if (
-        pessoa1PodeAdotar &&
-        !pessoa2PodeAdotar &&
-        animaisAdotadosPessoa1.length < 3
-      )
+      const pessoa1PodeAdotar =
+        pessoa1BrinquedosCorretos && animaisAdotadosPessoa1.length < 3;
+      const pessoa2PodeAdotar =
+        pessoa2BrinquedosCorretos && animaisAdotadosPessoa2.length < 3;
+
+      if (pessoa1PodeAdotar && !pessoa2PodeAdotar)
         animaisAdotadosPessoa1.push(animal);
-      if (
-        !pessoa1PodeAdotar &&
-        pessoa2PodeAdotar &&
-        animaisAdotadosPessoa2.length < 3
-      )
+      if (!pessoa1PodeAdotar && pessoa2PodeAdotar)
         animaisAdotadosPessoa2.push(animal);
     });
 
@@ -81,6 +87,7 @@ class AbrigoAnimais {
   }
 
   verificaBrinquedosCorretos(brinquedosMostrados, animal, animaisJaAdotados) {
+    //Interpretei que a regra 3 significa que se um gato e outro animal desejam os mesmos brinquedos, apenas um pode ser adotado (o que vier primeiro na ordem de consideração)
     if (animal.especie === 'gato' && animaisJaAdotados.length > 0) {
       for (const animalAdotado of animaisJaAdotados) {
         if (
@@ -105,7 +112,8 @@ class AbrigoAnimais {
       (brinquedo) => !brinquedosJaComGato.includes(brinquedo)
     );
 
-    //Interpretei que a regra 6 significa que Loco não se importa com a ordem dos brinquedos se outro animal já foi adotado pela mesma pessoa antes dele
+    /*Como os animais devem ser considerados na ordem especificada, interpretei que a regra 6 significa que Loco aceita os brinquedos fora de ordem
+    apenas se outro animal já foi adotado pela mesma pessoa antes dele, pois se ele vier antes ainda não há a informação de que ele vai ter companhia (ou não)*/
     if (animal.nome === 'Loco' && animaisJaAdotados.length > 0) {
       if (
         animal.brinquedosFavoritos.every((brinquedo) =>
@@ -135,16 +143,13 @@ class AbrigoAnimais {
     } else return false;
   }
 
-  verificaBrinquedoInvalido(brinquedos) {
-    const brinquedosValidos = new Set(
-      animaisDisponiveis.reduce(
-        (acumulador, animal) => [...acumulador, ...animal.brinquedosFavoritos],
-        []
-      )
-    );
+  verificaBrinquedoInvalido(brinquedosPessoa1, brinquedosPessoa2) {
+    for (const brinquedo of brinquedosPessoa1) {
+      if (!BRINQUEDOS_VALIDOS.has(brinquedo)) return true;
+    }
 
-    for (const brinquedo of brinquedos) {
-      if (!brinquedosValidos.has(brinquedo)) return true;
+    for (const brinquedo of brinquedosPessoa2) {
+      if (!BRINQUEDOS_VALIDOS.has(brinquedo)) return true;
     }
 
     return false;
